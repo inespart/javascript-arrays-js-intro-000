0b67128f26ad837852e1338d91361acbbbd5bf98
Ines P <ines.part@gmx.at>
2019-03-14 11:18:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,10 +23,15 @@ function accessElementInArray(array, index) {
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  array.shift(array);
+  array.shift();
   return array;
 }
 
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+  return array;
 }
