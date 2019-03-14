ce7e26f9b16b5e7a62ba73fda24e4b9e4a1e7f12
Ines P <ines.part@gmx.at>
2019-03-14 11:15:20 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -26,3 +26,8 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift(array);
   return array;
 }
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1);
+  return array;
+}
